07ef772512d52d89577dc395aef0a2517f7fa52d
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2022-03-05 00:23:12 +0300

    Create view for daily report. Allows search and export report to xls file

--- src/main/java/allteran/voyage/service/ExcelService.java
+++ src/main/java/allteran/voyage/service/ExcelService.java
@@ -1,26 +1,42 @@
 package allteran.voyage.service;
 
+import allteran.voyage.domain.PayType;
 import allteran.voyage.domain.Ticket;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import allteran.voyage.domain.TicketType;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 public class ExcelService {
-    private static final String XLSX_TEMPLATE_DIR = "src" + File.separator + "main" + File.separator + "resources" + File.separator +"static"+File.separator +"files" + File.separator;
-    private static final String DAILY_REPORT_TEMPLATE_NAME = "KASSOVIY_OTCHET.xlsx";
+    @Value("${paytype.id.card}")
+    private String PAYTYPE_ID_CARD;
+    @Value("${paytype.id.cash}")
+    private String PAYTYPE_ID_CASH;
 
-    public ByteArrayInputStream generateDailyReport(@NotNull List<Ticket> tickets) throws IOException {
+    private final PayTypeService payTypeService;
+
+    private static final String XLSX_TEMPLATE_DIR = "src" + File.separator + "main" + File.separator + "resources" + File.separator +"static"+File.separator +"files" + File.separator;
+    private static final String DAILY_REPORT_TEMPLATE_NAME = "KASSOVIY_OTCHET.xls";
+
+    @Autowired
+    public ExcelService(PayTypeService payTypeService) {
+        this.payTypeService = payTypeService;
+    }
+
+    //TODO: TEST THIS MF
+    public ByteArrayInputStream generateDailyReport(@NotNull List<Ticket> tickets, LocalDate reportDate) throws IOException {
         File currentDir = new File(".");
         String path = currentDir.getAbsolutePath().substring(0, currentDir.getAbsolutePath().length() - 1) +
                 XLSX_TEMPLATE_DIR + DAILY_REPORT_TEMPLATE_NAME;
@@ -38,11 +54,61 @@ public class ExcelService {
 
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        String subheader = dateFormatter.format(LocalDate.now()) + " НОРД-АВИА  МЕРИДИАН  авиабилеты";
-
+        String subheader = dateFormatter.format(reportDate) + " НОРД-АВИА  МЕРИДИАН  авиабилеты";
         mainSheet.getRow(1).getCell(0).setCellValue(subheader);
 
-        return null;
+        //prepare some font color modifications for subheader or regular text
+        Font typeHeaderFont = workbook.createFont();
+        typeHeaderFont.setFontName("Arial Cyr");
+        typeHeaderFont.setColor(HSSFColor.HSSFColorPredefined.RED.getIndex());
+        typeHeaderFont.setBold(true);
+
+        TicketType type = new TicketType();
+        int row = 5;
+        for (int i = 0; i < tickets.size(); i++) {
+            Ticket ticket = tickets.get(i);
+            if(!tickets.get(i).getType().equals(type)) {
+                //describe case for situation when we have to write type name first
+                type = ticket.getType();
+                Cell typeCell = mainSheet.getRow(row).getCell(4);
+                //setup type style to subheader
+                CellStyle style = workbook.createCellStyle();
+                style.setFont(typeHeaderFont);
+                style.setAlignment(HorizontalAlignment.CENTER);
+                typeCell.setCellStyle(style);
+                //now we can set value
+                typeCell.setCellValue(ticket.getType().getName());
+                row ++;
+            }
+            //now set ticket number to cell
+            mainSheet.getRow(row).getCell(2).setCellValue(ticket.getTicketNumber());
+            //set issue date to cell
+            mainSheet.getRow(row).getCell(3).setCellValue(dateFormatter.format(ticket.getIssueDate()));
+            //set route
+            mainSheet.getRow(row).getCell(4).setCellValue(ticket.getFlightRoute());
+            //set tariff
+            mainSheet.getRow(row).getCell(5).setCellValue(ticket.getTariffPrice());
+            //set tax YQ
+            mainSheet.getRow(row).getCell(6).setCellValue(ticket.getTaxYQPrice());
+            //set tax RU/YR
+            mainSheet.getRow(row).getCell(7).setCellValue(ticket.getTaxRUYRPrice());
+
+            //set total price depends on pay type (card or cash)
+            PayType cardPayType = payTypeService.findById(Long.valueOf(PAYTYPE_ID_CARD), new PayType());
+            if(ticket.getPayType().equals(cardPayType)) {
+                mainSheet.getRow(row).getCell(8).setCellValue(ticket.getTotalPrice());
+            } else {
+                mainSheet.getRow(row).getCell(9).setCellValue(ticket.getTotalPrice());
+            }
+            //set comment
+            mainSheet.getRow(row).getCell(11).setCellValue(ticket.getComment());
+            row++;
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        workbook.write(outputStream);
+
+        return new ByteArrayInputStream(outputStream.toByteArray());
 
     }
 }
--- src/main/java/allteran/voyage/ui/view/DailyReportView.java
+++ src/main/java/allteran/voyage/ui/view/DailyReportView.java
@@ -1,17 +1,21 @@
 package allteran.voyage.ui.view;
 
 import allteran.voyage.domain.Ticket;
+import allteran.voyage.service.ExcelService;
 import allteran.voyage.service.TicketService;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.LitRenderer;
@@ -19,24 +23,41 @@ import com.vaadin.flow.data.renderer.LocalDateRenderer;
 import com.vaadin.flow.function.SerializableComparator;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import javax.annotation.security.PermitAll;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 @Route(value = "daily-report", layout = MainView.class)
 @PageTitle("Ежедневный отчет | VOYAGE")
 @PermitAll
 public class DailyReportView extends Div {
     private final TicketService ticketService;
+    private final ExcelService excelService;
+
     private final Button searchButton;
+    private final Button generateReportButton;
 
     private Grid<Ticket> grid = new Grid<>(Ticket.class, false);
+    private List<Ticket> ticketList;
+
+    private ByteArrayInputStream dailyReport;
+    private DatePicker dateFilter = new DatePicker("Дата отчета");
 
     @Autowired
-    public DailyReportView(TicketService ticketService) {
+    public DailyReportView(TicketService ticketService, ExcelService excelService) {
         this.ticketService = ticketService;
+        this.excelService = excelService;
         searchButton = new Button("Найти", VaadinIcon.SEARCH.create());
-        Button generateReportButton = new Button("Экспортировать отчет", VaadinIcon.ARROW_DOWN.create());
+        generateReportButton = new Button("Экспортировать отчет", VaadinIcon.ARROW_DOWN.create());
 
         searchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         generateReportButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
@@ -46,13 +67,43 @@ public class DailyReportView extends Div {
         buttonsLayout.getElement().setAttribute("aria-label", "Profile");
         buttonsLayout.getStyle().set("margin-inline-start", "auto").set("padding", "5px");
 
-        generateReportButton.addClickListener(e -> Notification.show("ReportButton"));
+        generateReportButton.addClickListener(e -> {
+            if(ticketList == null || ticketList.isEmpty() || dateFilter.isEmpty()) {
+                Notification.show("За выбранную Вами дату не было продано ни одного билета").addThemeVariants(NotificationVariant.LUMO_ERROR);
+            } else {
+                generateReport(ticketList, dateFilter.getValue());
+                StreamResource resource = new StreamResource("KASSOVIY_OTCHET.xls", () -> new ByteArrayInputStream(dailyReport.readAllBytes()));
+                StreamRegistration registration = VaadinSession.getCurrent().getResourceRegistry().registerResource(resource);
+                UI.getCurrent().getPage().setLocation(registration.getResourceUri());
+                Notification.show("Готово! Сейчас начнется загрузка отчета").addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            }
+        });
+
+
 
         add(createToolbar(createFilter(), buttonsLayout));
 
         add(createGridLayout());
         createGridColumns();
     }
+
+
+    private void generateReport(List<Ticket> tickets, LocalDate reportDate) {
+        try {
+            dailyReport = excelService.generateDailyReport(tickets, reportDate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public HttpEntity<ByteArrayResource> downloadReport() {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(new MediaType("application", "force-download"));
+//        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=KASSOVY_OTCHET.xlsx");
+//
+//        return new HttpEntity<>(new ByteArrayResource(dailyReport.readAllBytes()), headers);
+//    }
+
 
     private Component createGridLayout() {
         grid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
@@ -94,7 +145,6 @@ public class DailyReportView extends Div {
     }
 
     private Component createFilter() {
-        DatePicker dateFilter = new DatePicker("Дата отчета");
 
         DatePicker.DatePickerI18n datePickerFormatter = new DatePicker.DatePickerI18n();
         datePickerFormatter.setDateFormat("dd.MM.yyyy");
@@ -104,7 +154,10 @@ public class DailyReportView extends Div {
 
         dateFilter.setWidth("30%");
 
-        searchButton.addClickListener(e -> grid.setItems(ticketService.findByIssueDate(dateFilter.getValue())));
+        searchButton.addClickListener(e -> {
+            ticketList = ticketService.findByIssueDate(dateFilter.getValue());
+            grid.setItems(ticketList);
+        });
 
         return dateFilter;
     }
